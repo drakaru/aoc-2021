8fafb81cd6a3a4dd6901b07c0a1dda20dabf989a
Alice <drakaru@gmail.com>
2021-12-07 12:45:37 +0000

    day6 & 7

---
+++ AoC2021/day06/main.cpp
@@ -0,0 +1,65 @@
+﻿// day06.cpp
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <array>
+#include <vector>
+#include <cstdint>
+#include <numeric>
+#include <charconv>
+#include <sstream>
+#include <bitset>
+
+using namespace std::string_literals;
+using LanternState = std::array<uint64_t, 9>;
+
+template<typename T>
+std::vector<T> extract(const std::string& csv, char delim = ',')
+{
+	std::vector<T> vec{};
+	std::stringstream aaaaa(csv);
+	std::string item;
+	while (std::getline(aaaaa, item, delim)) {
+		T n;
+		std::from_chars(item.data(), item.data() + item.size(), n);
+		vec.push_back(n);
+	}
+	return vec;
+}
+
+uint64_t simulate(const LanternState& initial_state, uint16_t days)
+{
+	LanternState state(initial_state);
+
+	for (uint16_t day = 0; day < days; day++)
+	{
+		LanternState copy{};
+		for (uint16_t t = 10; t < 19; t++)
+		{
+			copy[t - 10] = state[t % 9];
+		}
+		copy[6] += state[0];
+
+		state = copy;
+	}
+
+	return std::accumulate(state.begin(), state.end(), 0ull);
+}
+
+int main()
+{
+	auto f = std::ifstream("day06.txt");
+	
+	std::string line;
+	std::getline(f, line);
+	
+	LanternState state{};
+
+	for (auto x : extract<uint16_t>(line))
+	{
+		state[x] += 1;
+	}
+
+	std::cout << "part 1: " << simulate(state, 80) << "\n";
+	std::cout << "part 2: " << simulate(state, 256) << "\n";
+}
---
+++ AoC2021/day07/main.cpp
@@ -0,0 +1,81 @@
+﻿// day07.cpp
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <array>
+#include <cstdint>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <charconv>
+#include <sstream>
+#include <iterator>
+
+using namespace std::string_literals;
+
+
+template<typename T>
+std::vector<T> extract(const std::string& csv, char delim = ',')
+{
+	std::vector<T> vec{};
+	std::stringstream aaaaa(csv);
+	std::string item;
+	while (std::getline(aaaaa, item, delim)) {
+		T n;
+		std::from_chars(item.data(), item.data() + item.size(), n);
+		vec.push_back(n);
+	}
+	return vec;
+}
+
+void part1(const std::vector<int>& input)
+{
+	// lazy 
+	auto low = *(std::min_element(input.cbegin(), input.cend()));
+	auto high = *(std::max_element(input.cbegin(), input.cend()));
+	auto lowest_tot = INT_MAX;
+	
+	for (auto x = low; x < high; x++)
+	{
+		auto tot = std::accumulate(input.cbegin(), input.cend(), 0, [x](auto s, auto crab) { return s + std::abs(x - crab); });
+		lowest_tot = std::min(lowest_tot, tot);
+	}
+
+	std::cout << "part1 = " << lowest_tot << "\n";
+}
+
+void part2(const std::vector<int>& input)
+{
+	auto low = *(std::min_element(input.cbegin(), input.cend()));
+	auto high = *(std::max_element(input.cbegin(), input.cend()));
+	auto lowest_tot = INT_MAX;
+
+	for (auto x = low; x < high; x++)
+	{
+		// lazy again :D
+		auto tot = std::accumulate(input.cbegin(), input.cend(), 0, [x](auto s, auto crab) { 
+			auto dist = std::abs(x - crab);
+			auto dtot = dist;
+			for (auto d = 0; d < dist; d++)
+			{
+				dtot += d;
+			}
+			return s + dtot;
+		});
+		lowest_tot = std::min(lowest_tot, tot);
+	}
+
+	std::cout << "part2 = " << lowest_tot << "\n";
+}
+
+int main()
+{
+	auto f = std::ifstream("day07.txt");
+	std::string line;
+	std::getline(f, line);
+	auto input = extract<int>(line);
+
+	part1(input);
+	part2(input);
+}
+
